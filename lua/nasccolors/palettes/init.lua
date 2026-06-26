d6a9779179b02d00de3b27cfa28c39636af031f4
Nasccped <pedro.main.tec@gmail.com>
2026-06-26 18:10:42 -0300

    chore: init palette module

---
+++ lua/nasccolors/palettes/init.lua
@@ -0,0 +1,10 @@
+local M = {}
+
+--- Returns a color palette based on a ColorVariant.
+--- @param variant ColorVariant
+M.get_palette = function(variant)
+  -- print usage to check if variant thing works
+  vim.print('let\'s get the ' .. variant .. ' color palette...')
+end
+
+return M
